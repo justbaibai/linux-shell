a5ccb792b9cd8d8f76c0fb6fff6d31dae4c52610
XiaoBai521 <1551859659@qq.com>
2018-07-08 14:30:18 +0800

    Add files via upload

---
+++ cobbler报错.bash
@@ -0,0 +1,17 @@
+69 67 80 端口起来没有
+
+
+/etc/cobbler/dhcp.template  这个别写错了   看看是不是一个段的  
+rsync会报错
+
+
+/etc/cobbler/settings
+check  会报错
+
+
+tftp没开  会报错pex32 什么的
+
+
+ks配置文件错了   还会没有boot   aru什么的
+
+
