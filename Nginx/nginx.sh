59a5a09b698b30e7dfa272e59d6f88aeedd121da
白白 <1551859659@qq.com>
2018-10-17 13:59:38 +0800

    Update nginx.sh

--- Nginx/nginx.sh
+++ Nginx/nginx.sh
@@ -2,7 +2,7 @@
  yum install gcc gcc-c++
  useradd -s /sbin/nologin -M nginx
  #不让nginx登陆不创建家目录  主进程是root启的  子进程用
- 
+ nginx -V 看以前编译的参数   -v看nginx版本 
  #必要的的前期准备   一般nginx并发都很大   他不是瓶颈   主要是数据库  他可以水平扩展  
  
  wget https://nginx.org/download/nginx-1.14.0.tar.gz
@@ -12,5 +12,7 @@
  ./configure --prefix=/usr/local/nginx-1.14.0 --user=nginx --group=nginx --with-http_stub_status_module --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_realip_module --with-http_flv_module --with-http_mp4_module  --with-pcre --with-pcre-jit 
  cd /usr/local/
  ln -s nginx-1.14.0 nginx
- make&&makeinstall
+ nginx -t 
+ #检查语法
+ make&&make install
  
