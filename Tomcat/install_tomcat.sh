72a73596fdcc01cf83cbaab1065bc23ad1c3c09f
白白 <1551859659@qq.com>
2018-12-03 13:01:42 +0800

    Create install_tomcat.sh

---
+++ Tomcat/install_tomcat.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz
+cd /usr/local/src && tar xf apache-tomcat-8.5.35.tar.gz
+mv  /usr/local/src/apache-tomcat-8.5.35 /usr/local/apache-tomcat-8.5.35
+ln -s /usr/local/apache-tomcat-8.5.35/ /usr/local/tomcat
+echo "
+#!/bin/sh
+PATH=\$PATH:/usr/local/tomcat/bin
+">>/etc/profile.d/tomcat.sh
+
+source /etc/profile.d/tomcat.sh
