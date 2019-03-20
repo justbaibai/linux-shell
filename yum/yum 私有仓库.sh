bd084939bf3e677c64dd80f7c825011a28467194
白白 <1551859659@qq.com>
2019-03-20 15:58:51 +0800

    Update yum 私有仓库.sh

--- yum/yum 私有仓库.sh
+++ yum/yum 私有仓库.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 cd /etc/yum.repos.d/
+mkdir bak
 mv CentOS* bak
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
