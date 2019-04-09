19e47c17c89456d9e00cbc9e8fc547f13cf5b988
白白 <1551859659@qq.com>
2019-04-09 10:28:19 +0800

    Update install_mysql-5.7.sh

--- Mysql/install_mysql-5.7.sh
+++ Mysql/install_mysql-5.7.sh
@@ -6,7 +6,7 @@ mv /usr/local/src/mysql-5.7.25-linux-glibc2.12-x86_64 /usr/local/mysql-5.7.25
 useradd -s /sbin/nologin -M mysql
 chown -R mysql.mysql  /usr/local/mysql-5.7.25/*
 yum -y install autoconf libaio-devel
-cp /usr/local/mysql-5.7.25/support-files/my-default.cnf /etc/my.cnf
+#cp /usr/local/mysql-5.7.25/support-files/my-default.cnf /etc/my.cnf
 /usr/local/mysql-5.7.25/bin/mysqld  --initialize --basedir=/usr/local/mysql-5.7.25/ --datadir=/usr/local/mysql-5.7.25/data/ --user=mysql
 #/usr/local/mysql-5.6.43/bin/mysqld_safe&
 cp /usr/local/mysql-5.7.25/support-files/mysql.server /etc/init.d/mysqld
