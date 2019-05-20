9db8429f34234aba28210846f6f09899d4b964dc
白白 <1551859659@qq.com>
2019-05-20 09:37:45 +0800

    Update install_redis.sh

--- Redis/安装/install_redis.sh
+++ Redis/安装/install_redis.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+wget http://download.redis.io/releases/redis-5.0.4.tar.gz -P /usr/local/
+cd /usr/local/
+tar xf redis-5.0.4.tar.gz
+make
+make install PREFIX=/usr/local/redis
+#或者带版本号ln -s  软连接
+#这个以后升级的时候只要修改软连接的指向就行了
+#或者直接用
+cd src/
 
+cp /usr/local/redis-5.0.4/redis.conf /usr/local/redis/bin/
+/usr/local/redis/bin/redis-server /usr/local/redis/bin/redis.conf
+ /usr/local/redis/bin/redis-cli -h 10.0.3.118
+ auth baibai
+ /usr/local/redis/bin/redis-cli -h 10.0.3.118 -a baibai shutdown
