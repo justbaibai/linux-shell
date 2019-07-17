dacb74bc89c4011df882d14c7f680419b214a0ec
白白 <1551859659@qq.com>
2019-07-17 16:48:19 +0800

    版本升级

--- Tomcat/install_tomcat.sh
+++ Tomcat/install_tomcat.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz
-cd /usr/local/src && tar xf apache-tomcat-8.5.35.tar.gz
-mv  /usr/local/src/apache-tomcat-8.5.35 /usr/local/apache-tomcat-8.5.35
-ln -s /usr/local/apache-tomcat-8.5.35/ /usr/local/tomcat
+wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.43/bin/apache-tomcat-8.5.43.tar.gz
+cd /usr/local/src && tar xf apache-tomcat-8.5.43.tar.gz
+mv  /usr/local/src/apache-tomcat-8.5.43 /usr/local/apache-tomcat-8.5.43
+ln -s /usr/local/apache-tomcat-8.5.43/ /usr/local/tomcat
 echo "
 #!/bin/sh
 PATH=\$PATH:/usr/local/tomcat/bin
 ">>/etc/profile.d/tomcat.sh
 
 source /etc/profile.d/tomcat.sh
+
