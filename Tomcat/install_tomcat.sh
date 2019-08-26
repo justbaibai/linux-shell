45926c90e57bad3cfa0f54ee1e57c6d720a09e04
白白 <1551859659@qq.com>
2019-08-26 09:02:03 +0800

    小版本升级

--- Tomcat/install_tomcat.sh
+++ Tomcat/install_tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.43/bin/apache-tomcat-8.5.43.tar.gz
+wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
 cd /usr/local/src && tar xf apache-tomcat-8.5.43.tar.gz
 mv  /usr/local/src/apache-tomcat-8.5.43 /usr/local/apache-tomcat-8.5.43
 ln -s /usr/local/apache-tomcat-8.5.43/ /usr/local/tomcat
