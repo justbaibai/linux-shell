68efbf476bf757e711e70b9ae1326097c8bf0342
白白 <1551859659@qq.com>
2019-08-26 09:17:52 +0800

    Update install_tomcat.sh

--- Tomcat/install_tomcat.sh
+++ Tomcat/install_tomcat.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
-cd /usr/local/src && tar xf apache-tomcat-8.5.43.tar.gz
-mv  /usr/local/src/apache-tomcat-8.5.43 /usr/local/apache-tomcat-8.5.43
-ln -s /usr/local/apache-tomcat-8.5.43/ /usr/local/tomcat
+cd /usr/local/src && tar xf apache-tomcat-8.5.45.tar.gz
+mv  /usr/local/src/apache-tomcat-8.5.45 /usr/local/apache-tomcat-8.5.45
+ln -s /usr/local/apache-tomcat-8.5.45/ /usr/local/tomcat
 echo "
 #!/bin/sh
 PATH=\$PATH:/usr/local/tomcat/bin
