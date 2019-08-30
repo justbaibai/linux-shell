f7a2b381d8539ff8daed2300372261150e0a0316
白白 <1551859659@qq.com>
2019-08-30 15:41:15 +0800

    Update 真实安装linux.sh

--- 安装物理机linux/真实安装linux.sh
+++ 安装物理机linux/真实安装linux.sh
@@ -7,5 +7,12 @@
 4将vmlinuz initrd=initrd.img inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 rd.live.check quiet  改为  vmlinuz initrd=initrd.img inst.stage2=hd:/dev/sdb1 quiet   ps：sdb1就是你看到的启动盘名称
 
 5别忘了net.ifnames=0 biosdevname=0 
-        
-        sssssssssssss
+
+
+32G 内存配置示例：
+
+```
+<Connector port="8080" protocol="HTTP/1.1"
+               connectionTimeout="20000" maxThreads="1000" minSpareThreads="60" maxSpareThreads="600"  acceptCount="120"
+               redirectPort="8443" URIEncoding="utf-8"/>
+```
