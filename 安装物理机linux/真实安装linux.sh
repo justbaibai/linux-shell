f0fe1729d8c78959f67d6caed653f78048d59522
白白 <1551859659@qq.com>
2019-08-30 15:34:09 +0800

    Update 真实安装linux.sh

--- 安装物理机linux/真实安装linux.sh
+++ 安装物理机linux/真实安装linux.sh
@@ -3,6 +3,8 @@
 2查看U盘启动盘的名称比如：sda，sdb，sdc  ps：label一列会显示Centos7等字样的
 
 3重启后到第三步界面按下TAB键
+123:
+
 ```
 4将vmlinuz initrd=initrd.img inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 rd.live.check quiet  改为  vmlinuz initrd=initrd.img inst.stage2=hd:/dev/sdb1 quiet   ps：sdb1就是你看到的启动盘名称
 ```
