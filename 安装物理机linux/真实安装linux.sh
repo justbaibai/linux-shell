7b76a5fc0145c7da3b90675266797fec6ad0cc21
白白 <1551859659@qq.com>
2019-08-30 15:39:44 +0800

    Update 真实安装linux.sh

--- 安装物理机linux/真实安装linux.sh
+++ 安装物理机linux/真实安装linux.sh
@@ -7,3 +7,5 @@
 4将vmlinuz initrd=initrd.img inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 rd.live.check quiet  改为  vmlinuz initrd=initrd.img inst.stage2=hd:/dev/sdb1 quiet   ps：sdb1就是你看到的启动盘名称
 
 5别忘了net.ifnames=0 biosdevname=0 
+        
+        sssssssssssss
