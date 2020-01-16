76a82f6d29c76b7e6705c64b7e393808f0ef7db0
白白 <1551859659@qq.com>
2020-01-16 10:59:39 +0800

    升级tar包

--- Tomcat/install_tomcat.sh
+++ Tomcat/install_tomcat.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
-cd /usr/local/src && tar xf apache-tomcat-8.5.45.tar.gz
-mv  /usr/local/src/apache-tomcat-8.5.45 /usr/local/apache-tomcat-8.5.45
-ln -s /usr/local/apache-tomcat-8.5.45/ /usr/local/tomcat
+wget -P /usr/local/src http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.50/bin/apache-tomcat-8.5.50.tar.gz
+cd /usr/local/src && tar xf apache-tomcat-8.5.50.tar.gz
+mv  /usr/local/src/apache-tomcat-8.5.50 /usr/local/apache-tomcat-8.5.50
+ln -s /usr/local/apache-tomcat-8.5.50/ /usr/local/tomcat
 echo "
 #!/bin/sh
 PATH=\$PATH:/usr/local/tomcat/bin
@@ -10,6 +10,6 @@ PATH=\$PATH:/usr/local/tomcat/bin
 
 useradd tomcat
 passwd tomcat
-chown -R tomcat.tomcat /usr/local/apache-tomcat-8.5.45 
+chown -R tomcat.tomcat /usr/local/apache-tomcat-8.5.50
 source /etc/profile.d/tomcat.sh
 
